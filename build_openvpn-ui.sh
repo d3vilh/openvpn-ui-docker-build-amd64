bb5a8352df4ab2f84fdac129d0afeabcd4e12ca2
d3vilh <d3vilh@gmail.com>
2023-02-01 09:22:36 +0200

    build optimization

--- build_openvpn-ui.sh
+++ build_openvpn-ui.sh
@@ -2,6 +2,10 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+# Set the home directory of the openvpn-ui project
+OVPN_UI_HOME="/home/admin/build/openvpn-ui"
+
+# Set the current directory
 OD=$PWD
 
 # go to script folder, required for below steps
@@ -9,12 +13,10 @@ CURDIR=${OD}/$(dirname $0)
 cd "${CURDIR}"
 # Build golang & bee environment
 docker build --platform="linux/amd64" -f Dockerfile-beego -t local/beego -t local/beego:latest .
-
 ./openvpn-ui-pack2.sh
 
 # Build OpenVPN-UI image
-
-PKGFILE="/home/admin/build/openvpn-ui/openvpn-ui.tar.gz"
+PKGFILE=$OVPN_UI_HOME"/openvpn-ui.tar.gz"
 cp -f $PKGFILE ./
 
 docker build -t local/openvpn-ui .
--- openvpn-ui-pack2.sh
+++ openvpn-ui-pack2.sh
@@ -2,6 +2,9 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+# Set the home directory of the openvpn-ui project
+OVPN_UI_HOME="/home/admin/build/openvpn-ui"
+
 # Run a docker container with the specified volume and environment variable, and execute the specified command
     # Mount the host directory "/home/philipp/build/openvpn-web-ui/" to the container directory "/go/src/github.com/adamwalach/openvpn-web-ui"
     # Set the GO111MODULE environment variable to 'auto'
@@ -9,7 +12,7 @@ set -e
     # Use the "local/beego-v8" image
     # Execute the command "cd /go/src/github.com/d3vilh/openvpn-ui/ && bee version && bee pack -exr='^vendor|^data.db|^build|^README.md|^docs'"
 time docker run \
-    -v "/home/admin/build/openvpn-ui/":/go/src/github.com/d3vilh/openvpn-ui \
+    -v "$OVPN_UI_HOME":/go/src/github.com/d3vilh/openvpn-ui \
     -e GO111MODULE='auto' \
     --rm \
     -w /usr/src/myapp \
